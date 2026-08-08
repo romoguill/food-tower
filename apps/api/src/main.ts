a433539ae1bb3f4856fd14323aca14d28c91bc39
Rodrigo Moguillansky <romoguill@gmail.com>
2026-08-08 20:57:00 -0300

    Setup nestjs bootstrap project

---
+++ apps/api/src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+  app.setGlobalPrefix('api');
+
+  const port = process.env.PORT ?? 3000;
+
+  await app.listen(port);
+
+  console.log(`API running on port ${port}`);
+}
+
+void bootstrap();
