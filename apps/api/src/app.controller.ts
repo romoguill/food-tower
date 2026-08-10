edc0f6ac5f21085b772328ad90b610edb735b22a
Rodrigo Moguillansky <romoguill@gmail.com>
2026-08-10 09:39:56 -0300

    Created db module

--- apps/api/src/app.controller.ts
+++ apps/api/src/app.controller.ts
@@ -1,10 +1,11 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
 import type { HealthCheckResponse } from '@food-tower/types';
+import { Controller, Get, Inject } from '@nestjs/common';
+import { NeonHttpDatabase } from 'drizzle-orm/neon-http';
+import * as schema from './db/schema';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(@Inject('DB') private db: NeonHttpDatabase<typeof schema>) {}
 
   @Get('health')
   health(): HealthCheckResponse {
@@ -12,5 +13,12 @@ export class AppController {
       status: 'ok',
       timestamp: new Date(),
     };
+  }
+
+  @Get('db-test')
+  async dbTest() {
+    const result = await this.db.select().from(schema.users);
+
+    return { users: result, count: result.length };
   }
 }
--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { DbModule } from './db/db.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), DbModule],
   controllers: [AppController],
   providers: [AppService],
 })
